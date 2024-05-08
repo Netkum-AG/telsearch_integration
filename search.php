e31bb0445e9d3ea197b38dd75721580f2acdb196
Pierre Anken <pierre.anken@netkum.ch>
2024-05-08 14:53:09 +0200

    fix php

--- search.php
+++ search.php
@@ -1,23 +1,22 @@
 <?php
 try {
-    $request_text = ''
+    $request_text = 'TealSearch integration: PHP Error';
     // Check parameters
-    if(isset($_GET['wms_app_token']) && isset($_GET['wms_hostname']) && isset($_GET['$telsearch_key']) && isset($_GET['request_text'])) {
+    if (isset($_GET['wms_app_token']) && isset($_GET['wms_hostname']) && isset($_GET['$telsearch_key']) && isset($_GET['request_text'])) {
         $wms_app_token = $_GET['wms_app_token'];
         $wms_hostname = $_GET['wms_hostname'];
         $telsearch_key = $_GET['telsearch_key'];
         $request_text = $_GET['request_text'];
 
-        if(isset($_GET['telsearch_phonebook'])){
+        if (isset($_GET['telsearch_phonebook'])) {
             $telsearch_phonebook = $_GET['telsearch_phonebook'];
-            $output = shell_exec("/var/www/telsearch_integration/main.py ".$request_text." ".$telsearch_key." ".$wms_hostname." ".$wms_app_token." ".$telsearch_phonebook." 2>&1");
+            $output = shell_exec("/var/www/telsearch_integration/main.py " . $request_text . " " . $telsearch_key . " " . $wms_hostname . " " . $wms_app_token . " " . $telsearch_phonebook . " 2>&1");
         } else {
-            $output = shell_exec("/var/www/telsearch_integration/main.py ".$request_text." ".$telsearch_key."  ".$wms_hostname." ".$wms_app_token." 2>&1");
+            $output = shell_exec("/var/www/telsearch_integration/main.py " . $request_text . " " . $telsearch_key . "  " . $wms_hostname . " " . $wms_app_token . " 2>&1");
         }
 
-        echo($output);
-
-    else{
+        echo $output;
+    } else {
         echo("TealSearch integration: Invalid parameters");
     }
 } catch (Exception $e) {
